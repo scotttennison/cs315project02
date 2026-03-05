447b5a75c313d4d547ccc7344f44788f93da817a
Scott Tennison <scotttennison@Scotts-14-Incher.local>
2026-03-05 12:28:00 -0800

    Initial project files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Project 2 starter code" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Check for one argument
+    if (argc != 2)
+    {
+        cerr << "Usage: ./proj2 <image_prefix>" << endl;
+        return 1;
+    }
+
+    // Get the prefix
+    string prefix = argv[1];
+
+    string mazeNum = prefix + ".txt";
+
+    ifstream file(mazeNum);
+
+    if (!file.is_open())
+    {
+        cerr << "Error: Could not open " << mazeNum << endl;
+        return 1;
+    }
+
+    
+    int width, height;
+
+    file >> width >> height;
+
+    cout << "Opened: " << mazeNum << endl;
+    cout << "Size: " << width << " x " << height << endl;
+
+    file.close();
+
     return 0;
 }
