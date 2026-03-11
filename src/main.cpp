ca7b1f1a75ba7e689318c18b3d86a88803c796dc
Scott Tennison <scotttennison@Scotts-14-Incher.local>
2026-03-10 19:33:08 -0700

    Added functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,138 @@
 
 using namespace std;
 
+void printMaze(vector<string> maze)
+{
+    for (int i = 0; i < maze.size(); i++)
+    {
+        cout << maze[i] << endl;
+    }
+}
+
+bool solveMaze(vector<string> &mazeRows, int width, int height)
+{
+    queue<pair<int, int>> q;
+
+    int startRow = 0, startCol = 0;
+
+    for (int i = 0; i < mazeRows.size(); i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (mazeRows[i][j] == 'S')
+            {
+                startRow = i;
+                startCol = j;
+            }
+        }
+    }
+
+    q.push(make_pair(startRow, startCol));
+
+    vector<vector<bool>> visited(height, vector<bool>(width, false));
+    vector<vector<pair<int, int>>> parent(height, vector<pair<int, int>>(width, {-1, -1}));
+
+    visited[startRow][startCol] = true;
+
+    bool solved = false;
+
+    while (!q.empty())
+    {
+        pair<int, int> current = q.front();
+        q.pop();
+        int r = current.first;
+        int c = current.second;
+        if (mazeRows[r][c] == 'E')
+        {
+            while (mazeRows[r][c] != 'S')
+            {
+                if (mazeRows[r][c] == '.')
+                {
+                    mazeRows[r][c] = '-';
+                }
+                pair<int, int> p = parent[r][c];
+                r = p.first;
+                c = p.second;
+            }
+            solved = true;
+            break;
+        }
+        // Checks up
+        if (r - 1 >= 0 && !visited[r - 1][c] && mazeRows[r - 1][c] != '#')
+        {
+            q.push(make_pair(r - 1, c));
+            visited[r - 1][c] = true;
+            parent[r - 1][c] = make_pair(r, c);
+        }
+        // Checks down
+        if (r + 1 < height && !visited[r + 1][c] && mazeRows[r + 1][c] != '#')
+        {
+            q.push(make_pair(r + 1, c));
+            visited[r + 1][c] = true;
+            parent[r + 1][c] = make_pair(r, c);
+        }
+        // Checks left
+        if (c - 1 >= 0 && !visited[r][c - 1] && mazeRows[r][c - 1] != '#')
+        {
+            q.push(make_pair(r, c - 1));
+            visited[r][c - 1] = true;
+            parent[r][c - 1] = make_pair(r, c);
+        }
+        // Check right
+        if (c + 1 < width && !visited[r][c + 1] && mazeRows[r][c + 1] != '#')
+        {
+            q.push(make_pair(r, c + 1));
+            visited[r][c + 1] = true;
+            parent[r][c + 1] = make_pair(r, c);
+        }
+    }
+    return solved;
+}
+
+bool validateMaze(vector<string> &mazeRows, int width, int height)
+{
+    if (mazeRows.size() != height)
+    {
+        cerr << "Error: Rows don't equal height" << endl;
+        return false;
+    }
+
+    int numS = 0, numE = 0;
+
+    for (int i = 0; i < mazeRows.size(); i++)
+    {
+        if (mazeRows[i].length() != width)
+        {
+            cerr << "Error: Columns don't equal width" << endl;
+            return false;
+        }
+        for (int j = 0; j < width; j++)
+        {
+            if (mazeRows[i][j] != '#' && mazeRows[i][j] != '.' && mazeRows[i][j] != 'S' && mazeRows[i][j] != 'E')
+            {
+                cerr << "Error: Invalid character" << endl;
+                return false;
+            }
+            if (mazeRows[i][j] == 'S') numS++;
+            if (mazeRows[i][j] == 'E') numE++;
+        }
+    }
+
+    if (numS != 1)
+    {
+        cerr << "Error: Must have exactly one S" << endl;
+        return false;
+    }
+
+    if (numE != 1)
+    {
+        cerr << "Error: Must have exactly one E" << endl;
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     // Check for one argument
@@ -56,147 +188,22 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (mazeRows.size() != height)
-    {
-        cerr << "Error: Rows don't equal height"
-             << endl;
+    if (!validateMaze(mazeRows, width, height))
         return 1;
-    }
 
-    int numS = 0, numE = 0;
-
-    for( int i = 0; i < mazeRows.size(); i++)
-    {
-        if (mazeRows[i].length() != width)
-        {
-            cerr << "Error: Columns don't equal width"
-                 << endl;
-            return 1;
-        }
-        for (int j = 0; j < width; j++)
-        {
-            if (mazeRows[i][j] != '#' && mazeRows[i][j] != '.' && mazeRows[i][j] != 'S' && mazeRows[i][j] != 'E')
-            {
-                cerr << "Error: Invalid character "
-                     << endl;
-                return 1;
-            }   
-            if(mazeRows[i][j] == 'S') numS ++;
-            if(mazeRows[i][j] == 'E') numE++;
-        }
-    }
-
-    if(numS != 1)
-    {
-        cerr << "Error: Must have exactly one S" << endl;
-        return 1;
-    }
-
-    if (numE != 1)
-    {
-        cerr << "Error: Must have exactly one E" << endl;
-        return 1;
-    }
-
-    for (int i = 0; i < mazeRows.size(); i++)
-    {
-        cout << mazeRows[i] << endl;
-    }
+    printMaze(mazeRows);
 
     cout << endl;
 
-    queue<pair<int, int>> q;
+    bool solved = solveMaze(mazeRows, width, height);
+    if (solved)
+        printMaze(mazeRows);
+    else
+        cout << "This maze has no solution." << endl;
 
-    int startRow = 0, startCol = 0;
-
-    for (int i = 0; i < mazeRows.size(); i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (mazeRows[i][j] == 'S')
-            {
-                startRow = i;
-                startCol = j;
-            }
-        } 
-    }
-
-    q.push(make_pair(startRow, startCol));
-
-    vector<vector<bool>> visited(height, vector<bool>(width, false));
-    vector<vector<pair<int,int>>> parent(height, vector<pair<int,int>>(width, {-1,-1}));
-
-
-    visited[startRow][startCol] = true;
-
-    bool solved = false;
-
-    while (!q.empty())
-    {
-        pair<int,int> current = q.front();
-        q.pop();
-        int r = current.first;
-        int c = current.second;
-        if (mazeRows[r][c] == 'E')
-        {
-            while (mazeRows[r][c] != 'S')
-            {
-                if (mazeRows[r][c] == '.')
-                {
-                    mazeRows[r][c]= '-';
-                    
-                }
-                pair<int, int> p = parent[r][c];
-                r = p.first;
-                c = p.second;
-            }
-            solved = true;
-            break;
-        }
-        // Checks up
-        if (r - 1 >= 0 && !visited[r - 1][c] && mazeRows[r - 1][c] != '#')
-        {
-            q.push(make_pair(r - 1, c));
-            visited[r - 1][c] = true;
-            parent[r - 1][c] = make_pair(r, c);
-        }
-        // Checks down
-        if (r + 1 < height && !visited[r + 1][c] && mazeRows[r + 1][c] != '#')
-        {
-            q.push(make_pair(r + 1, c));
-            visited[r + 1][c] = true;
-            parent[r + 1][c] = make_pair(r, c);
-        }
-        // Checks left
-        if (c - 1 >= 0 && !visited[r][c-1] && mazeRows[r][c-1] != '#')
-        {
-            q.push(make_pair(r, c - 1));
-            visited[r][c - 1] = true;
-            parent[r][c - 1] = make_pair(r, c);
-        }
-        // Check right
-        if (c + 1 < width && !visited[r][c + 1] && mazeRows[r][c +1] != '#')
-        {
-            q.push(make_pair(r, c + 1));
-            visited[r][c + 1] = true;
-            parent[r][c + 1] = make_pair(r, c);
-        }
-    }
-
-    if (solved == true)
-    {
-        for (int i = 0; i < mazeRows.size(); i++)
-        {
-            cout << mazeRows[i] << endl;
-        }
-    }
-    else 
-    {
-        cout << "This maze has no solution. " << endl;
-    }
-    
-
-        file.close();
+    file.close();
 
     return 0;
 }
+
+
